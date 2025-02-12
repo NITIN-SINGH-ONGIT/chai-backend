fe723c1b0e1b43669f9d89d083850b8de8ef236e
NITIN-SINGH-ONGIT <nitinsingh231304@gmail.com>
2025-02-12 14:11:28 +0530

    api reaponce

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import express from "express"; // Import Express
 import connectDB from "./db/index.js";
+
 dotenv.config({
-    path: '/env'
-})
+    path: "/env",
+});
+
+const app = express(); // Initialize the app
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT || 8000}`);
+        });
+    })
+    .catch((err) => {
+        console.log("DB_CONNECTION : FAILED!", err);
+    });
+
+    
+// Handle app errors  if any errors occur at the application level, they'll be logged, and the   error will be thrown.
+app.on("error", (error) => {
+    console.log("ERROR", error);
+    throw error;
+});
+
+export default app; // Export app if needed elsewhere
+
 
 
 
