7afca22be940f07affa62ae38c59131db7ebb2da
NITIN-SINGH-ONGIT <nitinsingh231304@gmail.com>
2025-02-19 09:16:50 +0530

    this is for file based routing

---
+++ app/about/page.js
@@ -0,0 +1,25 @@
+import React from 'react'
+// import Navbar from '../component/Navbar'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-8xl text-red-400 m-3'>This is about page</h1>
+
+      <h2>Problem solved by Next.js : = =  </h2>
+      <br />
+      <ul>
+        <li>File based routing</li>
+        <li>Additinol feature like router form next/navigation</li>
+        <li>Optimized rendring</li>
+      </ul>
+      <br />
+      <br />
+      <br />
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, reiciendis accusamus! Distinctio ullam cum fugit, excepturi aliquid odit eius ex, suscipit eveniet natus, modi necessitatibus quia quaerat cumque alias eaque neque impedit! Consectetur, odit iusto. Aperiam est possimus consequatur fugit, asperiores illo sit nemo sapiente totam ipsam voluptatem libero assumenda officia rem ipsum sint obcaecati deserunt autem aut incidunt quasi tempore atque nobis nisi. Neque quae rerum placeat laudantium! Sunt corporis quas perferendis soluta perspiciatis eius? Nesciunt expedita atque doloremque qui consequuntur aperiam dicta aliquid vel numquam? Sequi neque beatae laboriosam. Voluptatem accusamus quod tempora iusto pariatur vel libero aut.</p>
+    </div>
+  )
+}
+
+export default page
---
+++ app/contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+       <h1 className='text-5xl'>This is contact page</h1>
+       <input type='email' placeholder='Enter your email' />
+    </div>
+  )
+}
+
+export default contact
